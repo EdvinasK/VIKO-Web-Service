689379becf54842d0c1f1b8f027b77802e72b2d5
EdvinasK <edvinas.kesminas@gmail.com>
2017-02-25 22:27:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var passport = require('passport');
 var Strategy = require('passport-local').Strategy;
+var soap = require('soap');
 var db = require('./db');
 
 
